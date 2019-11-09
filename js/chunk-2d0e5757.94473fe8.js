af9bef2317f9aca887595eebbae641c84f4bd452
arupratoncse <arupraton.cse@gmail.com>
2019-11-09 21:54:27 +0900

    Redirect to name

---
+++ js/chunk-2d0e5757.94473fe8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5757"],{9527:function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div")},u=[],a=e("8aa5"),c=e.n(a),r={name:"logout",created:function(){var n=this;c.a.auth().signOut().then((function(){n.$router.go({name:"login"})})).catch((function(n){return console.log(n)}))}},i=r,l=e("2877"),s=Object(l["a"])(i,o,u,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e5757.94473fe8.js.map
